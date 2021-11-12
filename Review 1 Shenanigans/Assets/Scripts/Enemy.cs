de2c6fbaec4e6b0c2ffd63dde33640bde5c331ce
Bug01000011Hunter <61810539+Bug01000011Hunter@users.noreply.github.com>
2021-11-12 14:47:58 +0700

    delete sunnyland, make mechanics, tilemap

---
+++ Review 1 Shenanigans/Assets/Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int maxHP = 3;
+    private int HP;
+    private void Start()
+    {
+        HP = maxHP;
+    }
+    public void TakeDamage(int damage)
+    {
+        HP -= damage;
+        if (HP <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Review 1 Shenanigans/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed = 2f;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 input;
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+        input.Normalize();
+        rb.velocity = input * speed;
+    }
+}
