2f828ab8d5c1ed009867149daaa3669bc8c07b12
Bug01000011Hunter <61810539+Bug01000011Hunter@users.noreply.github.com>
2021-11-12 15:31:53 +0700

    dash

--- Review 1 Shenanigans/Assets/Scripts/PlayerMovement.cs
+++ Review 1 Shenanigans/Assets/Scripts/PlayerMovement.cs
@@ -6,18 +6,42 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
     public float speed = 2f;
+    private Vector2 lastDirection;
+    public float range = 5f;       //5 unit
+    public float dashSpeed = 10f;  //10 unit/sec
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        canMove = true;
     }
 
+    bool canMove;
     // Update is called once per frame
     void Update()
     {
+        if (!canMove) return;
         Vector2 input;
         input.x = Input.GetAxisRaw("Horizontal");
         input.y = Input.GetAxisRaw("Vertical");
         input.Normalize();
+        if (input != Vector2.zero)
+        {
+            lastDirection = input;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            canMove = false;
+
+            rb.velocity = lastDirection * dashSpeed;
+            StartCoroutine(DelayDash());
+
+            return;
+        }
         rb.velocity = input * speed;
+    }
+    IEnumerator DelayDash()
+    {
+        yield return new WaitForSeconds(range / dashSpeed);
+        canMove = true;
     }
 }
