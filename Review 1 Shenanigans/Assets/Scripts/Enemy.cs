26f4b32b8e30ba754d66fde31e24d50bf5297982
Bug01000011Hunter <61810539+Bug01000011Hunter@users.noreply.github.com>
2021-11-12 15:00:58 +0700

    stuff

--- Review 1 Shenanigans/Assets/Scripts/Enemy.cs
+++ Review 1 Shenanigans/Assets/Scripts/Enemy.cs
@@ -6,9 +6,23 @@ public class Enemy : MonoBehaviour
 {
     public int maxHP = 3;
     private int HP;
+    private Rigidbody2D rb;
+    private Transform player;
+    public float speed = 5f;
+    public float maxSpeed = 5f;
     private void Start()
     {
         HP = maxHP;
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+    private void FixedUpdate()
+    {
+        rb.AddForce((player.position - transform.position).normalized * speed);
+        if (rb.velocity.sqrMagnitude > maxSpeed * maxSpeed)
+        {
+            rb.velocity = rb.velocity.normalized * maxSpeed;
+        }
     }
     public void TakeDamage(int damage)
     {
